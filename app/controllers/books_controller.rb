53f5b1985196e3e5dd80d0fee672a1fd971876c1
Hamm-Coder <176797595+Hamm-Coder@users.noreply.github.com>
2024-11-03 05:42:06 +0000

    commit3_Create_Read

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,15 +1,18 @@
 class BooksController < ApplicationController
-  
-  def index
+
+  def index #index_new
     @books = Book.all
+    # 新規投稿(new)部分
+    # Viewへ渡すためのインスタンス変数に空のModelオブジェクト（Book）を生成する。
+    @book = Book.new
   end
-  
-  def new
-    # Viewへ渡すためのインスタンス変数に空のModelオブジェクト（List）を生成する。
-    @list = List.new
-  end
-  
-  def create
+
+#  def new
+#    # Viewへ渡すためのインスタンス変数に空のModelオブジェクト（Book）を生成する。
+#    @book = Book.new
+#  end
+
+  def create #create
     #1.&2. データを受取り、新規登録するためのインスタンス変数（Modelオブジェクト用のparams（List）、オブジェクト）を作成する。
     @book = Book.new(book_params)
     #3. データをデータベースに保存するためのsaveメソッド実行
@@ -17,18 +20,34 @@ class BooksController < ApplicationController
       #4-1.フラッシュメッセージを定義し、詳細画面へリダイレクト（登録完了し、トップ画面へリダイレクト）
       flash[:notice] = "投稿に成功しました。"
       redirect_to book_path(@book.id)
-
     else
     #4-2 指定アクション名で同一コントローラー内の別アクションのviewを表示する。
       flash.now[:alert] = "投稿に失敗しました。" #キーをalertに変更
       render :new
+
     end
   end
 
-  def show
-    
+  def show #show
+    @book = Book.find(params[:id])
   end
 
-  def edit
+  def update #update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
+  def destroy #destroy
+    book = Book.find(params[:id])# ListModelから、Paramsで、idカラム指定の該当IDのレコードを1件取得
+    book.destroy #データ(レコード)削除
+    redirect_to '/books' #リダイレクトbooks View
+  end
+
+  private
+  # ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title, :body,)
+
   end
 end
